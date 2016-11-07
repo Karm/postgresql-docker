7e1f4a3cfc067a46690b30e32da484e403f84139
Michal Karm Babacek <karm@email.cz>
2016-11-07 01:22:14 +0100

    POSTGRES_MAX_CONNECTIONS

--- additional-configs.sh
+++ additional-configs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's/max_connections = [0-9]*/max_connections = 500/g' /dbdata/postgresql.conf
+sed -i "s/max_connections = [0-9]*/max_connections = ${POSTGRES_MAX_CONNECTIONS}/g" /dbdata/postgresql.conf
